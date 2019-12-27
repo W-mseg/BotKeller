74938cdb2c30c25579a04db018daf77676762fd4
512LouisWicket <louis.wicket@gmail.com>
2019-12-27 14:48:58 +0100

    Refactor

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client()
+const Discord = require("discord.js");
+const bot = new Discord.Client();
 
+// fs pour File System, module natif de node.js qui permet la manipulation de fichiers
+const fs = require("fs");
 
-bot.on('message', function (message){
-    
-        if      (message.content ==='!cours')
-            {message.delete();
-            message.author.send('https://github.com/W-mseg/coursKeller');} 
-        else if (message.content ==='!veille') 
-            {message.delete();
-             message.author.send('https://github.com/becodeorg/The-Watch/blob/master/CRL-Keller-1.18/agenda.md');} 
-        else if ( message.content === '!liste')
-            {message.delete();
-             message.author.send('voici la liste des commandes : !veille , !cours , !liste');}  
-        else if (message.content ==='!bonjour') 
-             {message.delete();
-                for(let bonj = 0 ; bonj < 100; bonj++)
-                {
-                    message.author.send('bonjour bonjour');} 
-                }
+// L'événement ready est déclenché lorsque le bot est démarré et prêt à être utilisé
+bot.on("ready", () => console.log(`${bot.user.username} est prêt à être utilisé`));
 
-    
-})
+// On définit un préfixe pour les commandes du bot
+const prefix = "!";
 
-bot.login('NjU1NDExNzI2NDEzNzI1NzM3.XfUgHQ.ZxnA5OwYJ0Fbs3ZuhrPD1tfyD1o')
+bot.on("message", function(message) {
+    if (message.content === prefix + "cours") {
+        message.delete();
+        message.author.send("https://github.com/W-mseg/coursKeller");
+    } else if (message.content === prefix + "veille") {
+        message.delete();
+        message.author.send("https://github.com/becodeorg/The-Watch/blob/master/CRL-Keller-1.18/agenda.md");
+    } else if ( message.content === prefix + "liste") {
+        message.delete();
+        message.author.send("voici la liste des commandes : !veille , !cours , !liste");
+    } else if (message.content === prefix + "bonjour") {
+        message.delete();
+        for (let bonj = 0; bonj < 100; bonj++) {
+            message.author.send("bonjour bonjour");
+        }
+    }
+});
+
+// Lecture du token pour connecter le bot
+bot.login(JSON.parse(fs.readFileSync("token.json")));
